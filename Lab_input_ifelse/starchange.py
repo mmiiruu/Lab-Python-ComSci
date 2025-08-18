95fed28385d3d5380dd42f4253ac7ea788008f7f
Sippx <amjustafly@gmail.com>
2025-08-18 14:59:30 +0700

    Update starchange.py

--- Lab_input_ifelse/starchange.py
+++ Lab_input_ifelse/starchange.py
@@ -1,4 +1,8 @@
 n = int(input())
-c_2 = input()
-c_3 = input()
-print((c_2 + c_3)  * (n//2)  + c_2  * (n % 2))
+a = input()
+b = input()
+
+pair = (a + b) * (n // 2)
+last = a * (n % 2)
+
+print(pair + last)
