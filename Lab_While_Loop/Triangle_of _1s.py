dc673ee584529c26eff901606d6fd8f804449b0e
Sippx <amjustafly@gmail.com>
2025-08-18 10:19:06 +0700

    Update Triangle_of _1s.py

--- Lab_While_Loop/Triangle_of _1s.py
+++ Lab_While_Loop/Triangle_of _1s.py
@@ -1,7 +1,20 @@
-n = int(input())   # รับจำนวนบรรทัด
-c = input()        # รับเครื่องหมายแทนดาว
+height = int(input())
 
-i = 1
-while i <= n:      # วนตั้งแต่บรรทัดที่ 1 จนถึง n
-    print(c * i)   # พิมพ์เครื่องหมาย c ซ้ำ i ครั้ง
-    i += 1         # เพิ่มตัวนับบรรทัด
+i = 0
+while i < height:
+    current_line = ""
+    
+    leading_spaces_count = 2*height - (2 + 2*i)
+    current_line += " " * leading_spaces_count
+    
+    if i == 0:
+        current_line += "1"
+    else:
+        current_line += "1"
+
+        inner_spaces_count = 3 + (i - 1) * 4
+        current_line += " " * inner_spaces_count
+        current_line += "1"
+    
+    print(current_line)
+    i += 1
