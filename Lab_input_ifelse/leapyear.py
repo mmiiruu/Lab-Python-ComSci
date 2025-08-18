2f5052bce077ed1fd0abe5e61d5cb39336b979de
Sippx <amjustafly@gmail.com>
2025-08-18 15:21:57 +0700

    Update leapyear.py

--- Lab_input_ifelse/leapyear.py
+++ Lab_input_ifelse/leapyear.py
@@ -1,11 +1,10 @@
 year = int(input("Enter year: "))
+
 if year <= 0:
     print("Invalid year.")
-elif year % 400 == 0:
-    print(f"{year} is a leap year.")
-elif year % 100 == 0:
-    print(f"{year} is not a leap year.")
-elif year % 4 == 0:
+    exit()
+    
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
     print(f"{year} is a leap year.")
 else:
     print(f"{year} is not a leap year.")
